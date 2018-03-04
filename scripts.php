334b6ab74ce6f0d0b2e36455cae588951e241949
Mauro Riva <lemariva@gmail.com>
2018-03-04 13:01:36 +0100

    database modified, and version updated

--- scripts.php
+++ scripts.php
@@ -7,20 +7,20 @@ return [
 		$util = $app['db']->getUtility();
 
 
-		if ($util->tableExists('@adsensekit_ads') === false) {
-			$util->createTable('@adsensekit_ads', function ($table) {
-				$table->addColumn('id', 'integer', ['unsigned' => true, 'length' => 10, 'autoincrement' => true]);
-				$table->addColumn('status', 'smallint');
-        $table->addColumn('priority', 'smallint');
-				$table->addColumn('title', 'string', ['length' => 255]);
-				$table->addColumn('slug', 'string', ['length' => 255]);
-				$table->addColumn('adformat', 'string', ['length' => 255]);
-        $table->addColumn('adlayoutkey', 'string', ['length' => 255]);
-        $table->addColumn('adclient', 'string', ['length' => 255]);
-        $table->addColumn('adslot', 'string', ['length' => 255]);
-        $table->addColumn('date', 'datetime', ['notnull' => false]);
-				$table->setPrimaryKey(['id']);
-        $table->addUniqueIndex(['slug'], '@ADSENSEKIT_SLUG');
+    if ($util->tableExists('@adsensekit_ads') === false) {
+      $util->createTable('@adsensekit_ads', function ($table) {
+            $table->addColumn('id', 'integer', ['unsigned' => true, 'length' => 10, 'autoincrement' => true]);
+            $table->addColumn('status', 'smallint');
+            $table->addColumn('priority', 'smallint', ['default' => 0]);
+            $table->addColumn('title', 'string', ['length' => 255]);
+            $table->addColumn('slug', 'string', ['length' => 255]);
+            $table->addColumn('adformat', 'string', ['length' => 255]);
+            $table->addColumn('adlayoutkey', 'string', ['length' => 255]);
+            $table->addColumn('adclient', 'string', ['length' => 255]);
+            $table->addColumn('adslot', 'string', ['length' => 255]);
+            $table->addColumn('date', 'datetime', ['notnull' => false]);
+            $table->setPrimaryKey(['id']);
+            $table->addUniqueIndex(['slug'], '@ADSENSEKIT_SLUG');
 			});
 		}
 
